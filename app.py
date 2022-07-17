3b90c6b6805ac1ae3cab8ad42f51a409fc3a6d89
Hirushaashinshana <95660986+Hirushaashinshana@users.noreply.github.com>
2022-07-17 19:41:53 +0530

    updated

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print('HEello')
+import requests
+
+def convert ():
+    what_currency = input('What currency do you want to convert? ')
+    which_currency = input('to which currency do you want to convert? ')
+    url = "https://binance43.p.rapidapi.com/ticker/24hr"
+
+    querystring = {"symbol":"BTCUSDT"}
+
+    headers = {
+        "X-RapidAPI-Key": "febae8becamshfd2559dd073bc98p13e8f9jsn0a0965f0f99c",
+        "X-RapidAPI-Host": "binance43.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
